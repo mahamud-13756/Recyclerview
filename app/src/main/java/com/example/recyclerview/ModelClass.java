fd55de96f24b2f169749ed9e8cce370894b18234
Mahamud_Mredha <mahamud15-13756@diu.edu.bd>
2022-01-24 20:35:08 +0600

    This is a ModelClas .Also called data class

---
+++ app/src/main/java/com/example/recyclerview/ModelClass.java
@@ -0,0 +1,28 @@
+package com.example.recyclerview;
+
+public class ModelClass {
+
+    private int imageview1;
+    private String textview;
+    private String textview2;
+    private String textview3;
+
+
+
+
+    public int getImageview1() {
+        return imageview1;
+    }
+
+    public String getTextview() {
+        return textview;
+    }
+
+    public String getTextview2() {
+        return textview2;
+    }
+
+    public String getTextview3() {
+        return textview3;
+    }
+}
